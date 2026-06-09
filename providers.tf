5b97664756af6f278c6f88c96f4dcf9fb6c3d482
Parthasarathi-jain-MCA <parthasarathi.panda.jain@gmail.com>
2026-06-09 12:16:21 +0530

    added providers

---
+++ providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
